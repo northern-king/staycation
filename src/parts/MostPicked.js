4896b74d678c371d7b221e7708e2f372c205c056
northern-king <62548150+northern-king@users.noreply.github.com>
2020-09-16 22:32:32 +0700

    add MostPickedo section

---
+++ src/parts/MostPicked.js
@@ -0,0 +1,50 @@
+import React from "react";
+import Button from "elements/Button";
+
+export default function MostPicked(props) {
+  return (
+    <section
+      className="container"
+      ref={props.refMostPicked}
+      style={{ height: 1000 }}
+    >
+      <h4 className="mb-5">Most Picked</h4>
+      <div className="container-grid">
+        {props.data.map((item, index) => {
+          return (
+            <div
+              key={`mostPicked-${index}`}
+              className={`item column-4${index === 0 ? " row-2" : " row-1"}`}
+            >
+              <div className="card card-featured">
+                <div className="tag">
+                  ${item.price}{" "}
+                  <span className="font-weight-light">per {item.unit}</span>
+                </div>
+                <figure className="img-wrapper">
+                  <img
+                    src={item.imageUrl}
+                    className="img-cover"
+                    alt={item.name}
+                  />
+                </figure>
+                <div className="meta-wrapper">
+                  <Button
+                    className="stretched-link d-block text-white"
+                    type="link"
+                    href={`/properties/${item._id}`}
+                  >
+                    <h5>{item.name}</h5>
+                  </Button>
+                  <span>
+                    {item.city}, {item.country}
+                  </span>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
+}
