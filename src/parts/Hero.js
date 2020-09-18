ac82c027e339cf29557d0f48c7c5a93b3db4e59d
northern-king <62548150+northern-king@users.noreply.github.com>
2020-09-18 15:55:18 +0700

    add Footer section  and react reaveal animation

--- src/parts/Hero.js
+++ src/parts/Hero.js
@@ -1,4 +1,7 @@
 import React from "react";
+import Fade from "react-reveal/Fade";
+import Pulse from "react-reveal/Pulse";
+
 import ImageHero from "assets/images/img-hero.jpg";
 import FrameHero from "assets/images/img-hero-frame.jpg";
 import IconCities from "assets/images/icons/ic_cities.svg";
@@ -16,85 +19,91 @@ export default function Hero(props) {
   }
 
   return (
-    <section className="container pt-4">
-      <div className="row align-items-center">
-        <div className="col-auto pr-5" style={{ width: 520 }}>
-          <h1 className="font-weight-bold line-height-1 mb-3">
-            Forget Busy Work,
-            <br /> Start Next Vacation
-          </h1>
-          <p className="font-weight-light mb-5 text-gray-500 w-75">
-            We provide what you need to enjoy your holiday with family. Time to
-            make another memorable moments.
-          </p>
-          <Button
-            className="btn px-5"
-            hasShadow
-            isPrimary
-            onClick={showMostPicked}
-          >
-            Show Me Now
-          </Button>
-          <div className="row" style={{ marginTop: 80 }}>
-            <div className="col-auto" style={{ marginRight: 35 }}>
-              <img
-                width="36"
-                height="36"
-                src={IconTraveler}
-                alt={`${props.data.travelers} Travelers`}
-              />
-              <h6 className="mt-3">
-                {numberFormat(props.data.travelers)}{" "}
-                <span className="text-gray-500 font-weight-light">
-                  travelers
-                </span>
-              </h6>
+    <Fade bottom>
+      <section className="container pt-4">
+        <div className="row align-items-center">
+          <div className="col-auto pr-5" style={{ width: 520 }}>
+            <h1 className="font-weight-bold line-height-1 mb-3">
+              Forget Busy Work,
+              <br /> Start Next Vacation
+            </h1>
+            <p className="font-weight-light mb-5 text-gray-500 w-75">
+              We provide what you need to enjoy your holiday with family. Time
+              to make another memorable moments.
+            </p>
+            <Pulse delay={700}>
+              <Button
+                className="btn px-5"
+                hasShadow
+                isPrimary
+                onClick={showMostPicked}
+              >
+                Show Me Now
+              </Button>
+            </Pulse>
+            <div className="row" style={{ marginTop: 80 }}>
+              <div className="col-auto" style={{ marginRight: 35 }}>
+                <img
+                  width="36"
+                  height="36"
+                  src={IconTraveler}
+                  alt={`${props.data.travelers} Travelers`}
+                />
+                <h6 className="mt-3">
+                  {numberFormat(props.data.travelers)}{" "}
+                  <span className="text-gray-500 font-weight-light">
+                    travelers
+                  </span>
+                </h6>
+              </div>
+              <div className="col-auto" style={{ marginRight: 35 }}>
+                <img
+                  width="36"
+                  height="36"
+                  src={IconTreasure}
+                  alt={`${props.data.treasures} Treasure`}
+                />
+                <h6 className="mt-3">
+                  {numberFormat(props.data.treasures)}{" "}
+                  <span className="text-gray-500 font-weight-light">
+                    treasure
+                  </span>
+                </h6>
+              </div>
+              <div className="col-auto">
+                <img
+                  width="36"
+                  height="36"
+                  src={IconCities}
+                  alt={`${props.data.cities} Cities`}
+                />
+                <h6 className="mt-3">
+                  {numberFormat(props.data.cities)}{" "}
+                  <span className="text-gray-500 font-weight-light">
+                    cities
+                  </span>
+                </h6>
+              </div>
             </div>
-            <div className="col-auto" style={{ marginRight: 35 }}>
+          </div>
+          <div className="col-6 pl-5">
+            <div style={{ width: 520, height: 410 }}>
               <img
-                width="36"
-                height="36"
-                src={IconTreasure}
-                alt={`${props.data.treasures} Treasure`}
+                src={ImageHero}
+                alt="Room with couches"
+                className="img-fluid position-absolute"
+                style={{ margin: "-30px 0 0 -30px", zIndex: 1 }}
               />
-              <h6 className="mt-3">
-                {numberFormat(props.data.treasures)}{" "}
-                <span className="text-gray-500 font-weight-light">
-                  treasure
-                </span>
-              </h6>
-            </div>
-            <div className="col-auto">
               <img
-                width="36"
-                height="36"
-                src={IconCities}
-                alt={`${props.data.cities} Cities`}
+                src={FrameHero}
+                alt="Room with couches"
+                className="img-fluid position-absolute"
+                style={{ margin: "0 -15px -15px 0" }}
               />
-              <h6 className="mt-3">
-                {numberFormat(props.data.cities)}{" "}
-                <span className="text-gray-500 font-weight-light">cities</span>
-              </h6>
             </div>
           </div>
         </div>
-        <div className="col-6 pl-5">
-          <div style={{ width: 520, height: 410 }}>
-            <img
-              src={ImageHero}
-              alt="Room with couches"
-              className="img-fluid position-absolute"
-              style={{ margin: "-30px 0 0 -30px", zIndex: 1 }}
-            />
-            <img
-              src={FrameHero}
-              alt="Room with couches"
-              className="img-fluid position-absolute"
-              style={{ margin: "0 -15px -15px 0" }}
-            />
-          </div>
-        </div>
-      </div>
-    </section>
+      </section>
+    </Fade>
   );
 }
